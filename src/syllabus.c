3ca120c71a784e7eac74cd7e93c5e10f4f6de21d
kabaotchunkoua <kabaotchunkoua@gmail.com>
2026-05-29 19:16:32 +0000

    complit the function for course creation and a safty to avoid overflow

--- src/syllabus.c
+++ src/syllabus.c
@@ -5,4 +5,28 @@
  * @author [Your Name]
  * @date 26-05-2026
  */
-v
+#include "../include/syllabus.h"
+#include<stdlib.h>
+#include<string.h>
+#include <stdio.h>
+
+//Helper to safely copy strings and avoid overflow
+
+    static void safe_strcpy(char* dest, const char* src, size_t max_len)
+{
+   if(!dest || !src) return;
+   strncpy(dest, src, max_len-1); 
+   dest[max_len -1 ] = '\0';
+}
+// Creat Course
+    Course* syllabus_create_course(const char title[]){
+        Course* new_course = malloc(sizeof(Course));
+        if(!new_course){
+            fprintf( stderr, "Error: failed to allocate Memory for Course\n");
+            return NULL;
+        }
+        safe_strcpy(new_course->title, title, MAX_TITLE);
+        new_course -> chapters = NULL;
+        new_course -> next = NULL;
+        return new_course;
+    }
